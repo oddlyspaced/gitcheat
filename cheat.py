d24b27f67518376f3726576b7fafa3bef38c7657
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-23 04:40:17 +0530

    cheat: Add sample method to open sign up page and fill in details

--- cheat.py
+++ cheat.py
@@ -5,6 +5,7 @@ from passwordgenerator import pwgenerator
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from config import Constants as cons
+from random_username.generate import generate_username
 
 api_id = cons.api_id
 api_hash = cons.api_hash
@@ -28,12 +29,32 @@ async def response_handler(event):
         for word in words:
             if fake_mail_domain in word:
                 fake_mail = word
-                print(fake_mail)
+                print("Fake email generated: " + fake_mail)
+                sign_up()
 
 def generate_email():
     client.send_message(id_fakemailbot, "/generate")
 
 
+# Github SignUp
+driver = webdriver.Firefox()
+link = "https://github.com/join"
+def sign_up():
+    global fake_mail
+    username = generate_username(1)[0]
+    email = fake_mail
+    password = fake_mail
+    driver.get(link)
+    # finding text fields
+    element_username = driver.find_element_by_id("user_login")
+    element_email = driver.find_element_by_id("user_email")
+    element_password = driver.find_element_by_id("user_password")
+    # sending details to text fields
+    element_username.send_keys(username)
+    element_email.send_keys(email)
+    element_password.send_keys(password)
+
 client.start()
 generate_email()
+# sign_up()
 client.run_until_disconnected()
