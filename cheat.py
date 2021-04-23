3f57ad498f4205f8ccbe3061c31b2c71d52183a9
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-23 05:30:10 +0530

    cheat: Add methods to automatically check verification and follow user

--- cheat.py
+++ cheat.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from config import Constants as cons
 from random_username.generate import generate_username
+import time
 
 api_id = cons.api_id
 api_hash = cons.api_hash
@@ -37,10 +38,17 @@ def generate_email():
 
 
 # Github SignUp
-driver = webdriver.Firefox()
+option = webdriver.ChromeOptions()
+# For ChromeDriver version 79.0.3945.16 or over
+option.add_argument('--disable-blink-features=AutomationControlled')
+#Open Browser
+driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=option)
 link = "https://github.com/join"
 def sign_up():
     global fake_mail
+    #####
+    # fake_mail = "gbbwphpm@hi2.in"
+    #####
     username = generate_username(1)[0]
     email = fake_mail
     password = fake_mail
@@ -53,6 +61,42 @@ def sign_up():
     element_username.send_keys(username)
     element_email.send_keys(email)
     element_password.send_keys(password)
+    # finding continue button
+    wait_for_verification()
+
+def wait_for_verification():
+    print("Waiting for verification...")
+    element_button = driver.find_element_by_id("signup_button")
+    while (element_button.get_attribute("disabled") != None):
+        time.sleep(1)
+    print("Verified!")
+    element_button.click()
+    try :
+        verify_join()
+    except:
+        follow_oddly()
+
+def verify_join():
+    print("Waiting for Join Github verification")
+    element_button = driver.find_element_by_id("signup_button")
+    while (element_button.get_attribute("disabled") != None):
+        time.sleep(1)
+    print("Verified and sign up done!")
+    follow_oddly()
+    # driver.get("https://github.com/oddlyspaced")
+
+def follow_oddly():
+    print("Following oddly")
+    driver.get("https://github.com/oddlyspaced")
+    # Follow oddlyspaced
+    # element_follow = driver.find_element_by_css_selector("[title^='Follow oddlyspaced']")
+    # element_follow = driver.find_element_by_xpath("//a[@title='Follow oddlyspaced']")
+    # element_follow = driver.find_element_by_xpath("//body/div[4]/main[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/span[1]/form[1]/input[2]")
+    # element_follow.click()
+    element_follow = driver.find_element_by_name("commit")
+    driver.execute_script("arguments[0].click();", element_follow)
+    driver.get("https://github.com/oddlyspaced")
+    print("Done")
 
 client.start()
 generate_email()
